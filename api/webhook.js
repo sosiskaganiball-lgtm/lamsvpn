8f1c5deeea0d564576555facae86a3f5b5b85933
sosiskaganiball-lgtm <sosiskaganiball@gmail.com>
2026-05-12 03:25:36 +0300

    webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
       const now = Date.now();
       const prev = user.expiry ? Number(user.expiry) : 0;
       user.expiry = Math.max(now, prev) + days * 86400000;
-      await put(`user:${email}`, JSON.stringify(user), { access: 'public' });
+      await put(`user:${email}`, JSON.stringify(user));
     }
   }
   res.sendStatus(200);
